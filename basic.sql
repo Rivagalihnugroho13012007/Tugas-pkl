738d85de1c504cfae218aa6fb806db03324d841c
Rivagalihnugroho13012007 <151481422+Rivagalihnugroho13012007@users.noreply.github.com>
2024-06-30 17:25:58 -0700

    Add files via upload

---
+++ basic.sql
@@ -0,0 +1,106 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 01 Jul 2024 pada 02.24
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `basic`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `account`
+--
+
+CREATE TABLE `account` (
+  `username` varchar(45) NOT NULL,
+  `password` varchar(250) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `role` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `account`
+--
+
+INSERT INTO `account` (`username`, `password`, `name`, `role`) VALUES
+('admin', 'admin', 'admin', 'admin'),
+('user', 'user', 'user', 'user');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `post`
+--
+
+CREATE TABLE `post` (
+  `idpost` int(11) NOT NULL,
+  `title` text NOT NULL,
+  `content` text NOT NULL,
+  `date` datetime NOT NULL,
+  `username` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `post`
+--
+
+INSERT INTO `post` (`idpost`, `title`, `content`, `date`, `username`) VALUES
+(1, 'aaa', 'bbn', '0000-00-00 00:00:00', 'admin');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `account`
+--
+ALTER TABLE `account`
+  ADD PRIMARY KEY (`username`);
+
+--
+-- Indeks untuk tabel `post`
+--
+ALTER TABLE `post`
+  ADD PRIMARY KEY (`idpost`),
+  ADD KEY `fk_post_account_idx` (`username`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `post`
+--
+ALTER TABLE `post`
+  MODIFY `idpost` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `post`
+--
+ALTER TABLE `post`
+  ADD CONSTRAINT `fk_post_account` FOREIGN KEY (`username`) REFERENCES `account` (`username`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
